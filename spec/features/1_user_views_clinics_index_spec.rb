43b4a1f02271d36f2fcc61e70270e5441a4ced23
LightningStairs <jenah.blitz@icloud.com>
2017-06-02 19:48:13 -0400

    starter code!

--- spec/features/1_user_views_clinics_index_spec.rb
+++ spec/features/1_user_views_clinics_index_spec.rb
@@ -10,21 +10,21 @@ describe "user views clinics" do
     visit clinics_path
   end
 
-  xit "lists the clinics" do
+  it "lists the clinics" do
     expect(page).to have_content("Eben's Totally Awesome Clinic")
     expect(page).to have_content("Amanda <3 Dogs")
     expect(page).to have_content("Mike's Breakfast Corner")
     expect(page).to have_content("Blitz of Baked Goods")
   end
 
-  xit "has links to each clinic" do
+  it "has links to each clinic" do
     expect(page).to have_link(nil, href: clinic_path(clinic1))
     expect(page).to have_link(nil, href: clinic_path(clinic2))
     expect(page).to have_link(nil, href: clinic_path(clinic3))
     expect(page).to have_link(nil, href: clinic_path(clinic4))
   end
 
-  xit "has a link to create a new clinic" do
+  it "has a link to create a new clinic" do
     expect(page).to have_link(nil, href: new_clinic_path)
   end
 end
--- spec/features/3_user_creates_a_clinic_spec.rb
+++ spec/features/3_user_creates_a_clinic_spec.rb
@@ -7,19 +7,19 @@ describe "user creates a clinic" do
     visit new_clinic_path
   end
 
-  xit "creates a new clinic with valid data" do
+  it "creates a new clinic with valid data" do
     fill_in("Title", with: "Blitz of Baked Goods")
     fill_in("Description", with: "Carbohydrates plus oven equals delicious.")
     fill_in("Offered at", with: "04/10/2017, 3:00 PM")
-    click_button("Create Clinic")
+    click_button("submit")
 
     expect(page).to have_content("Clinic was successfully created.")
     expect(Clinic.count).to eq(clinic_count + 1)
   end
 
-  xit "fails to create a clinic with invalid data" do
+  it "fails to create a clinic with invalid data" do
     fill_in("Description", with: "Carbohydrates plus oven equals delicious.")
-    click_button("Create Clinic")
+    click_button("submit")
 
     expect(find_field("clinic_description").value).to eq("Carbohydrates plus oven equals delicious.")
     expect(page).to have_content("Title can't be blank")
