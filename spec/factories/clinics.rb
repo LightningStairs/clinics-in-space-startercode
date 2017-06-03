bc5229f23ee39c4b4bfc00f7df0e5bd6a8c77b39
LightningStairs <jenah.blitz@icloud.com>
2017-06-02 20:12:07 -0400

    fix factory sequencing

--- spec/factories/clinics.rb
+++ spec/factories/clinics.rb
@@ -2,7 +2,7 @@
 #
 # FactoryGirl.define do
 #   factory :clinic do
-#     sequence :title { |n| "Clinic #{n}" }
+#     sequence(:title) { |n| "Clinic #{n}" }
 #     description "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
 #     offered_at { 7.days.from_now }
 #   end
